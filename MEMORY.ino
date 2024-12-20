dbd1273c66168e79df71270f61073fa49bfcaca5
canulasu <jakarandainnovation@gmail.com>
2024-12-20 09:12:41 +0100

    Add files via upload

---
+++ MEMORY.ino
@@ -0,0 +1,19 @@
+void memory1() {
+  Serial.println("MEMORY1");
+}
+
+void memory2() {
+  Serial.println("MEMORY2");
+}
+
+void memory3() {
+  Serial.println("MEMORY3");
+}
+
+void memory4() {
+  Serial.println("MEMORY4");
+}
+
+void memory5() {
+  Serial.println("MEMORY5");
+}
